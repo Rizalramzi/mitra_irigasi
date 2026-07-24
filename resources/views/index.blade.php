6239bb8fe9efd34a2142fcdcb10e7e95b1d59668
Rizalramzi <ri126661@gmail.com>
2026-07-24 23:33:52 +0700

    add: index page and modify chatbot to /chatbot

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Home | Mitra Irigasi</title>
+</head>
+<body>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-4xl font-bold">Home</h1>
+        <a href="/chatbot" class="text-blue-500">Chatbot</a>
+    </div>  
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('app');
+    return view('index');
+});
+
+Route::get('/chatbot', function () {
+    return view('chatbot');
 });
